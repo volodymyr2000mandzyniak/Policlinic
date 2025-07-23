d3322f1bb43a6cfa29594e4d8c59ecb92dafe112
volodymyr2000mandzyniak <volodimirmandzyniak@gmail.com>
2025-07-23 17:03:35 +0300

    Add Category model and associate with Doctor

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_22_164508) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_23_135410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +33,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_22_164508) do
     t.date "date_of_birth"
     t.string "address"
     t.string "phone"
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_doctors_on_category_id"
     t.index ["email"], name: "index_doctors_on_email", unique: true
     t.index ["phone"], name: "index_doctors_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
@@ -49,4 +57,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_22_164508) do
     t.index ["phone"], name: "index_patients_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "doctors", "categories"
 end
