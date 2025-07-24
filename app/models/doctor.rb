480904ebe180e4a958f922b21cf44ab19db01aab
volodymyr2000mandzyniak <volodimirmandzyniak@gmail.com>
2025-07-24 13:05:19 +0300

    Replace email authentication with phone_number: updating views and models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,17 @@ class Doctor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable, authentication_keys: [:phone]
 
   belongs_to :category
+
+  validates :phone, presence: true, uniqueness: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
